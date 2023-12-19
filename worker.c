f9c25940c7bd4b82be16221990318aa7e3fd8c8f
ajahgm <115183767+ajahgm@users.noreply.github.com>
2023-12-18 22:30:38 -0600

    Update worker.c

--- worker.c
+++ worker.c
@@ -1,5 +1,7 @@
-// worker.c
-// Ahmed Ahmed CS4760 10/12/23
+// Ahmed Ahmed 
+// CS4760
+// Project 3(REDO)
+// 12/18/23
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,34 +14,43 @@
 #define SHM_KEY 12345
 #define MSG_QUEUE_KEY 1234
 
+// Message structure used for message queue communication
 typedef struct {
     long mtype;
     int data;
 } Message;
 
+// Clock structure to hold the shared clock information
 typedef struct {
     int seconds;
     int nanoseconds;
 } Clock;
 
 int main(int argc, char *argv[]) {
+    // Ensure correct number of command-line arguments
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <TermTimeS> <TermTimeNano>\n", argv[0]);
         exit(1);
     }
+
+    // Parse termination time from command-line arguments
     int termTimeS = atoi(argv[1]);
     int termTimeNano = atoi(argv[2]);
 
+    // Initial output upon starting
     printf("WORKER PID:%d PPID:%d Called with oss: TermTimeS: %d TermTimeNano: %d\n",
            getpid(), getppid(), termTimeS, termTimeNano);
 
+    // Attach to shared memory for the system clock
     int clockShmId = shmget(SHM_KEY, sizeof(Clock), 0666);
     Clock *sharedClock = (Clock *)shmat(clockShmId, NULL, 0);
 
+    // Access the message queue created by oss
     int msgQueueId = msgget(MSG_QUEUE_KEY, 0666);
 
     int lastReportedSecond = -1;
     while (1) {
+        // Receive a message from oss
         Message msg;
         if (msgrcv(msgQueueId, &msg, sizeof(msg.data), getpid(), 0) == -1) {
             perror("msgrcv failed");
@@ -47,6 +58,7 @@ int main(int argc, char *argv[]) {
         }
         printf("WORKER PID:%d PPID:%d --Received message\n", getpid(), getppid());
 
+        // Output the time and check if a second has passed since last report
         if (sharedClock->seconds > lastReportedSecond) {
             printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d TermTimeS: %d TermTimeNano: %d\n",
                    getpid(), getppid(), sharedClock->seconds, sharedClock->nanoseconds, termTimeS, termTimeNano);
@@ -54,6 +66,7 @@ int main(int argc, char *argv[]) {
             lastReportedSecond = sharedClock->seconds;
         }
 
+        // Check if the termination condition is met
         if (sharedClock->seconds >= termTimeS && sharedClock->nanoseconds >= termTimeNano) {
             printf("WORKER PID:%d PPID:%d SysClockS: %d SysclockNano: %d --Terminating\n",
                    getpid(), getppid(), sharedClock->seconds, sharedClock->nanoseconds);
@@ -62,7 +75,7 @@ int main(int argc, char *argv[]) {
                 perror("msgsnd failed");
                 exit(1);
             }
-            break;
+            break; // Exit the loop and terminate
         } else {
             msg.data = 1; // Indicating continuation
             if (msgsnd(msgQueueId, &msg, sizeof(msg.data), 0) == -1) {
@@ -72,6 +85,7 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    // Detach from the shared memory segment before terminating
     shmdt(sharedClock);
     return 0;
 }
