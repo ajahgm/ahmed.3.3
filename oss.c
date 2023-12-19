01fb78c495dc57fa0d5964620c8bbd7c250c12e6
ajahgm <115183767+ajahgm@users.noreply.github.com>
2023-12-18 22:29:32 -0600

    Update oss.c

--- oss.c
+++ oss.c
@@ -1,5 +1,7 @@
-// oss.c
-// Ahmed Ahmed CS4760 10/12/23
+// Ahmed Ahmed 
+// CS4760
+// Project 3(REDO)
+// 12/18/23
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,20 +13,19 @@
 #include <sys/msg.h>
 #include <time.h>
 #include <stdarg.h>
-#include <string.h>
 
 #define MAX_CHILDREN 20
 #define SHM_KEY 12345
 #define MSG_QUEUE_KEY 1234
 #define TIMEOUT_SECONDS 60
 
-// Message structure
+// Define the structure for messages in the message queue
 typedef struct {
     long mtype;
     int data;
 } Message;
 
-// Process Control Block (PCB) structure
+// Define the structure for Process Control Blocks (PCBs)
 typedef struct {
     int occupied;
     pid_t pid;
@@ -32,17 +33,18 @@ typedef struct {
     int startNano;
 } PCB;
 
-// Clock structure
+// Define the structure for the shared clock
 typedef struct {
     int seconds;
     int nanoseconds;
 } Clock;
 
-// Global variables
+// Global variables for shared clock, process table, and message queue
 Clock *sharedClock;
 PCB processTable[MAX_CHILDREN];
 int msgQueueId;
 
+// Function to increment the shared clock
 void incrementClock(Clock *clock) {
     clock->nanoseconds += 100000000;
     if (clock->nanoseconds >= 1000000000) {
@@ -51,27 +53,31 @@ void incrementClock(Clock *clock) {
     }
 }
 
+// Signal handler for SIGINT (CTRL-C)
 void handleSigint(int signum) {
     printf("\nReceived SIGINT (CTRL-C). Terminating gracefully...\n");
     exit(0);
 }
 
+// Signal handler for real-life timeout
 void handleTimeout(int signum) {
     printf("Program timed out. Cleaning up...\n");
     exit(1);
 }
 
+// Function to log output both to the console and a log file
 void logOutput(FILE *logFile, const char *format, ...) {
     va_list args;
     va_start(args, format);
-    vprintf(format, args);
+    vprintf(format, args); // Print to console
     va_end(args);
     va_start(args, format);
-    vfprintf(logFile, format, args);
+    vfprintf(logFile, format, args); // Print to log file
     va_end(args);
-    fflush(logFile);
+    fflush(logFile); // Flush the log file to ensure all data is written
 }
 
+// Function to launch child processes (workers)
 void launchChildProcesses(int maxChildren, FILE *logFile) {
     static int childrenLaunched = 0;
     if (childrenLaunched < maxChildren) {
@@ -81,8 +87,8 @@ void launchChildProcesses(int maxChildren, FILE *logFile) {
             exit(1);
         } else if (pid == 0) {
             // Child process
-            char *args[] = {"./worker", NULL}; // Add more arguments as needed
-            execvp(args[0], args);
+            char *args[] = {"./worker", NULL}; // Replace with actual arguments
+            execvp(args[0], args); // Execute the worker program
             perror("Exec failed");
             exit(1);
         } else {
@@ -97,6 +103,7 @@ void launchChildProcesses(int maxChildren, FILE *logFile) {
     }
 }
 
+// Function to check and handle terminated child processes
 void checkAndHandleTerminatedChildren(int *totalChildrenTerminated, FILE *logFile) {
     pid_t terminatedPid;
     int status;
@@ -108,7 +115,7 @@ void checkAndHandleTerminatedChildren(int *totalChildrenTerminated, FILE *logFil
                 (*totalChildrenTerminated)++;
                 logOutput(logFile, "Child process with PID %d terminated\n", terminatedPid);
 
-                // Example of sending a message using msgsnd
+                // Send a message using msgsnd (for example)
                 Message msg;
                 msg.mtype = terminatedPid; // Using PID as message type
                 msg.data = 0; // Data to be sent
@@ -119,44 +126,43 @@ void checkAndHandleTerminatedChildren(int *totalChildrenTerminated, FILE *logFil
 }
 
 int main(int argc, char *argv[]) {
-    // Signal handling setup
+    // Set up signal handlers
     signal(SIGINT, handleSigint);
     signal(SIGALRM, handleTimeout);
     alarm(TIMEOUT_SECONDS);
 
-    // Shared memory setup
+    // Set up shared memory for the clock
     int clockShmId = shmget(SHM_KEY, sizeof(Clock), IPC_CREAT | 0666);
     sharedClock = (Clock *)shmat(clockShmId, NULL, 0);
 
-    // Message queue setup
+    // Set up the message queue
     msgQueueId = msgget(MSG_QUEUE_KEY, IPC_CREAT | 0666);
 
-    // Open log file
+    // Open a log file for output
     FILE *logFile = fopen("oss_log.txt", "w");
     if (logFile == NULL) {
         perror("Error opening log file");
         exit(1);
     }
 
-    // Command-line arguments handling
-    int maxChildren = MAX_CHILDREN; // Default value
-    // Add logic here to parse command-line arguments if needed
+    // Placeholder for handling command-line arguments
+    int maxChildren = MAX_CHILDREN; // Default value for max children
 
     int totalChildrenTerminated = 0;
 
-    // Main loop
+    // Main loop of the program
     while (totalChildrenTerminated < maxChildren) {
         incrementClock(sharedClock);
         launchChildProcesses(maxChildren, logFile);
         checkAndHandleTerminatedChildren(&totalChildrenTerminated, logFile);
 
-        // Log current state
+        // Log the current state of the system
         logOutput(logFile, "Current system time: %d seconds, %d nanoseconds\n", 
                   sharedClock->seconds, sharedClock->nanoseconds);
 
         for (int i = 0; i < MAX_CHILDREN; i++) {
             if (processTable[i].occupied) {
-                logOutput(logFile, "Entry %d: PID %d, Start Time %d.%d\n", 
+              logOutput(logFile, "Entry %d: PID %d, Start Time Seconds: %d, Start Time Nanoseconds: %09d\n",   
                           i, processTable[i].pid, 
                           processTable[i].startSeconds, processTable[i].startNano);
             } else {
@@ -165,10 +171,10 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // Cleanup
+    // Cleanup resources before terminating
     fclose(logFile);
     shmdt(sharedClock);
-    msgctl(msgQueueId, IPC_RMID, NULL); // Remove message queue
+    msgctl(msgQueueId, IPC_RMID, NULL); // Remove the message queue
 
     return 0;
 }
